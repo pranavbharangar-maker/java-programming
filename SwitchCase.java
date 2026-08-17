6f504b9535c1a8bc64b0b039c52cd6a8964ed967
pranavbharangar-maker <pranavbharangar@gmail.com>
2026-08-17 15:17:39 +0530

    swich case program

--- SwitchCase.java
+++ SwitchCase.java
@@ -4,30 +4,14 @@ public class SwitchCase {
         char ch = 'a';
 
         switch (ch) {
-           case 'a':
-                System.out.println("sunday");
-                break;
-            case 'b':
-                System.out.println("monday");
-                break;
-            case 'c':
-                System.out.println("tuesday");
-                break;
-            case 'd':
-                System.out.println("wednesday");
-                break;    
-            case 'e':
-                System.out.println("thursday");
-                break;    
-            case 'f':
-                System.out.println("friday");
-                break;    
-            case 'g':
-                System.out.println("saturday");
-                break;
-            default:
-                System.out.println("invalid day");
-                break;        
+           case 'a' -> System.out.println("sunday");
+            case 'b' -> System.out.println("monday");
+            case 'c' -> System.out.println("tuesday");
+            case 'd' -> System.out.println("wednesday");
+            case 'e' -> System.out.println("thursday");
+            case 'f' -> System.out.println("friday");
+            case 'g' -> System.out.println("saturday");
+            default -> System.out.println("invalid day");        
         }
     }
 }

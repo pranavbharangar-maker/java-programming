eb0429004d47c5a9048875cb7c978c9fc52d7132
pranavbharangar-maker <pranavbharangar@gmail.com>
2026-08-22 22:05:38 +0530

    modified

--- UserPass.java
+++ UserPass.java
@@ -1,17 +1,27 @@
-import java.io.Console;
-
 public class UserPass {
-    public static void main(String[] args) {
-        
-        Console console = System.console();
-        
-        if (console == null) {
-            System.out.println("Console is not available.");
-            return;
+    public static void main(String [] args){
+       java.io.Console console=System.console();
+       int attempts=3;
+       if(console== null){
+        System.out.println("Console is not available");
+        return ;
+       }
+    //    System.out.println("password entered successfully" + pass);
+       while(attempts>0){
+     char [] password=console.readPassword("enter passowrd: ");
+       int pass=Integer.parseInt(new String (password));
+        if(pass==1234){
+             System.out.println("correct passowerd");
         }
-        
-        char[] password = console.readPassword("Enter Password: ");
-        
-        System.out.println("Password entered successfully!");
+        else{
+            System.out.println("incorrect password");
+            attempts--;
+            System.out.println("attempts left "+attempts);
+        }
+
+       }
+       if(attempts==0){
+        System.out.println("system locked");
+       }
     }
 }
